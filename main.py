7e892d391865b907f8cbc891e28ec071995e2a2a
onceagainarise <154758475+onceagainarise@users.noreply.github.com>
2025-09-05 05:16:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 from typing import Annotated, List
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-from langchain.chat_models import init_chat_model
 from typing_extensions import TypedDict
 from pydantic import BaseModel, Field
 import os
